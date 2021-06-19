f6fa44c1d59ae1d893f55f956117b767c0b3eb82
renanluizmacedo <renanluizmacedo@gmail.com>
2021-06-18 22:54:05 -0300

    list clientes e enderecos

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Models\Cliente;
+use App\Models\Endereco;
+
+Route::get('/clientes', function () {
+    $clientes = Cliente::all();
+    foreach($clientes as $c){
+        echo "<p>ID: ".$c->id."</p>";
+        echo "<p>Nome: ".$c->nome."</p>";
+        echo "<p>Telefone: ".$c->telefone."</p>";
+
+        $e = Endereco::where('cliente_id',$c->id)->first();
+
+
+        echo "<p>Rua: ".$e->rua."</p>";
+        echo "<p>Numero: ".$e->numero."</p>";
+        echo "<p>Bairro: ".$e->bairro."</p>";
+        echo "<p>UF: ".$e->uf."</p>";
+        echo "<p>CEP: ".$e->cep."</p>";
+
+
+        echo "<hr>";
+    }
+});
+
+Route::get('/enderecos', function () {
+    $ends = Endereco::all();
+    foreach($ends as $e){
+
+        echo "<p>ID Cliente: ".$e->cliente_id."</p>";
+        echo "<p>Rua: ".$e->rua."</p>";
+        echo "<p>Numero: ".$e->numero."</p>";
+        echo "<p>Bairro: ".$e->bairro."</p>";
+        echo "<p>UF: ".$e->uf."</p>";
+        echo "<p>CEP: ".$e->cep."</p>";
+
+        echo "<hr>";
+    }
+});
